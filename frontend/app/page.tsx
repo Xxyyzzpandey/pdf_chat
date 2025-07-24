5fae8cc69a8e09092626a367ba24fc3d11e34193
ankit <ujjawalankit56023@gmail.com>
2025-07-24 15:20:38 +0530

    file uploads added

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import FileUploadComponents from "./components/fileUpload";
 
 export default function Home() {
   return (
     <div>
-      
+      <div className="min-h-screen w-screen flex">
+        <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center">
+            <FileUploadComponents/>
+        </div>
+      </div>
     </div>
   );
 }
